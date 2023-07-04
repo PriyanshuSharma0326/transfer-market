267aa2403bbf7683ad7cc5809c05d27e1516428a
PriyanshuSharma0326 <ps940016@gmail.com>
2023-07-05 00:26:49 +0530

    Initialize Project

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import PlayerCard from "./PlayerCard";
+
+export default function App() {
+    return (
+        <div className="App">
+            <PlayerCard />
+        </div>
+    );
+}
---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayerCard() {
+    return (
+        <div className='player-card-container'>
+            <img src='' alt='' />
+        </div>
+    );
+}
+
+export default PlayerCard;
